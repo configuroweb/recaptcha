600ea9afd33729202a4ef1d8e2615e63c27297cc
Mauricio Sevilla Britto <msevillab@gmail.com>
2020-08-03 11:32:09 -0500

    Se modifican los keys de recaptcha

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 return [
-	'client-key' => '6LfevbgZAAAAABRRQSt33P2ejF36ydMxQg_NSo6Z',
-	'secret-key' => '6LfevbgZAAAAAK_qdiu51ro67UWcEPTejP_j6Qst'
+	'client-key' => 'xx',
+	'secret-key' => 'xx'
 ];
